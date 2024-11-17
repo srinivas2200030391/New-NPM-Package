fc48af01c1c5d4898acae17c12fb8cf1ec20c494
Srinivas <vasus4990@gmail.com>
2024-11-17 22:26:54 +0530

    New NPM Package

---
+++ src/hooks/counter.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+export default function counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return { count, increment, decrement };
+}
